9c118907f038f6acce1f6dc4bd44ecded69f17c6
Liano <hailyduong@gmail.com>
2017-09-20 21:05:03 +0700

    Fix: Redux > If there is no Chrome Redux Extension

--- readable/src/index.js
+++ readable/src/index.js
@@ -13,11 +13,17 @@ import {Provider} from "react-redux";
 import reducers from "./reducers/reducers";
 import ReduxThunk from 'redux-thunk';
 
+const reduxDevTool = window.__REDUX_DEVTOOLS_EXTENSION__
+	? window.__REDUX_DEVTOOLS_EXTENSION__()
+	: (createStore) => {
+		return createStore
+	};
+
 const store = createStore(
 	reducers,
 	compose(
 		applyMiddleware(ReduxThunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		reduxDevTool
 	)
 );
 
